c59c79ff11db2c200a5bfe96b0f3f6c5041edaef
Ahmed Omara <ahmedhfmo@gmail.com>
2026-03-14 18:03:46 +0200

    create note item model

---
+++ lib/models/note_item_model.dart
@@ -0,0 +1,13 @@
+class NoteItemModel {
+  final String title;
+  final String subTitle;
+  final int color;
+  final String date;
+
+  NoteItemModel({
+    required this.title,
+    required this.subTitle,
+    required this.color,
+    required this.date,
+  });
+}
